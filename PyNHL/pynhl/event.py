bb332783411274b94cf317ae50fbb61db5229646
Ryan Smith <rmsmith12@gmail.com>
2020-01-19 10:08:03 -0500

    Inching along, need to re-work logic of leveraging Event class inside Game class, which should have been the intention from the beginning

--- PyNHL/pynhl/event.py
+++ PyNHL/pynhl/event.py
@@ -1,10 +1,8 @@
 class Event:
-    """
-    Events are shots, hits, faceoffs, takeaways, giveaways, goals, penalties
-    """
-
-    def __init__(self, player_for, team, type_of_event, period, time, score, x_loc=None, y_loc=None):
+    def __init__(self, player_for=None, player_against=None, team=None, type_of_event=None, period=None, time=None,
+                 score=None, x_loc=None, y_loc=None):
         self.player_for = player_for
+        self.player_against = player_against
         self.team_of_player = team
         self.type_of_event = type_of_event
         self.period = period
@@ -13,6 +11,8 @@ class Event:
         self.y_loc = y_loc
         self.score = score  # tuple of (away_goals,home_goals) transform to -> Tied, Leading, Trailing
         # self.state = 3v5, 4v5, 3v4, 3v3,4v4,5v5, 5v3,5v4,6v5
+        # self.gwg = True/False
+        # self.empty_net = True/False
 
     def __eq__(self, other):
         return self.type_of_event == other.type_of_event and self.period == other.type_of_event \
@@ -24,3 +24,88 @@ class Event:
     def __str__(self):
         return ("{},{},{},{},{},{},{},{}".format(self.player_for, self.team_of_player, self.type_of_event, self.period,
                                                  self.time, self.x_loc, self.y_loc))
+    def get_type(self, event):
+        """
+        Return the type of event from NHL API
+        ie "result": {"event": "Faceoff"}
+        """
+        self.e_type = event["result"]["event"]
+        return self
+
+    def get_players(self, event):
+        """
+        Return the player who DID the event
+        [0] is player who did the action, [1] is the player who received the action
+        Missed shots do not have the 2nd player, goals/other shots do
+        """
+        if 'Blocked' in self.e_type:
+            # API Puts the person who blocked the shot, before the person who shot the shot
+            p_for, p_against = event['players'][1]['player']['fullName'], event['players'][0]['player']['fullName']
+        elif 'Missed' in self.e_type or 'Giveaway' in self.e_type or "Takeaway" in self.e_type or "Penalty" in self.e_type:
+            # Missed shots do not a second player tracked (goalie etc)
+            # Takeaways or Giveaways do not track who it was from / given to
+            p_for, p_against = event['players'][0]['player']['fullName'], None
+        else:
+            # Shooter , Goalie
+            # Hitter, Hittee
+            p_for, p_against = event['players'][0]['player']['fullName'], event['players'][1]['player']['fullName']
+        return p_for, p_against
+
+    def get_team(self, event):
+        """
+        Returns the team of the player who DID the event
+        abbreviated (BUF) format not full (Buffalo Sabres)
+        """
+        e_team = event['team']['triCode']
+        return
+
+    def get_period(self, event):
+        """
+        Returns the period when the event occurred
+        """
+        e_period = event['about']['period']
+        return e_period
+
+    def get_time(self, event):
+        """
+        Returns the time, in seconds, when the event occurred
+        MM:SS -> SS
+        """
+        temp = datetime.datetime.strptime(event['about']['periodTime'], "%M:%S").time()
+        temp = int(datetime.timedelta(minutes=temp.minute, seconds=temp.second).total_seconds())
+        return temp
+
+    def get_score(self, event):
+        """
+        Adds the score at the time of the event
+        """
+        self.e_score = event['about']['goals']['away'], event['about']['goals']['home']
+        return self
+
+    def get_x(self, event):
+        """
+        Return x coordinate from event
+        """
+        self.e_x = event['coordinates']['x']
+        return self
+
+    def get_y(self, event):
+        """
+        Return y value from event
+        """
+        self.e_y = event['coordinates']['y']
+        return self
+
+    def retrieve_events_in_game(self):
+        """
+        Parse self.json_data and retrieve all events reported in the game
+        Helper function for each type of event, since each have their own little quirks
+        """
+        events = self.json_data['liveData']['plays']['allPlays']
+        for event in events:
+            self.e_type = event['result']['event']  # Type of event
+            if self.e_type in self.TRACKED_EVENTS:
+                self.events_in_game.add(self.parse_event(event))
+            elif self.e_type not in NOT_TRACKED_EVENTS:
+                print(self.e_type)
+        return self.events_in_game
--- PyNHL/pynhl/game.py
+++ PyNHL/pynhl/game.py
@@ -14,90 +14,6 @@ NOT_TRACKED_EVENTS = {
 }
 
 
-def get_type(event):
-    """
-    Return the type of event from NHL API
-    ie "result": {"event": "Faceoff"}
-    """
-    return event["result"]["event"]
-
-
-def get_players(event):
-    """
-    Return the player who DID the event
-    """
-    return event['players'][0]['player']['fullName']
-
-
-def get_team(event):
-    """
-    Returns the team of the player who DID the event
-    abbreviated (BUF) format not full (Buffalo Sabres)
-    """
-    return event['team']['triCode']
-
-
-def get_period(event):
-    """
-    Returns the period when the event occurred
-    """
-    return event['about']['period']
-
-
-def get_time(event):
-    """
-    Returns the time, in seconds, when the event occurred
-    MM:SS -> SS
-    """
-    temp = datetime.datetime.strptime(event['about']['periodTime'], "%M:%S").time()
-    return int(datetime.timedelta(minutes=temp.minute, seconds=temp.second).total_seconds())
-
-
-def get_score(event):
-    """
-    Adds the score at the time of the event
-    """
-    return event['about']['goals']['away'], event['about']['goals']['home']
-
-
-def get_x(event):
-    """
-    Return x coordinate from event
-    """
-    return event['coordinates']['x']
-
-
-def get_y(event):
-    """
-    Return y value from event
-    """
-    return event['coordinates']['y']
-
-
-def parse_shot(event):
-    """
-    Parse a shot event from the NHL API game data
-    """
-    e_type = get_type(event)
-    e_player_for = get_players(event)
-    e_team = get_team(event)
-    e_period = get_period(event)
-    e_time = get_time(event)
-    e_score = get_score(event)
-    e_x = get_x(event)
-    e_y = get_y(event)
-    return Event(e_player_for, e_team, e_type, e_period, e_time, e_score, e_x, e_y)
-
-
-def parse_faceoff(event):
-    """
-    Parse a faceoff event from the NHL API game data
-    """
-    return Event(player_for=get_players(event), team=get_team(event), type_of_event=get_type(event),
-                 period=get_period(event), time=get_time(event), score=get_score(event), x_loc=get_x(event),
-                 y_loc=get_y(event))
-
-
 class Game:
     # Game will have Players who will have shifts and each shift can/will have event(s)
     '''
@@ -105,16 +21,16 @@ class Game:
     And parse out the necessary data (players, events)
     Which will then be used to generate Player/Event classes
     '''
-    # "Hit",
-    # "Giveaway",
-    # "Goal",
-    # "Missed Shot",
-    # "Penalty",
-    # "Takeaway",
-    # "Blocked Shot"
     TRACKED_EVENTS = {
-        "Shot": parse_shot,
-        "Faceoff": parse_faceoff,
+        "Shot",
+        "Faceoff",
+        "Giveaway",
+        "Takeaway",
+        "Penalty",
+        "Missed Shot",
+        "Blocked Shot",
+        "Goal",
+        "Hit"
     }
 
     def __init__(self, json_input):
@@ -124,14 +40,15 @@ class Game:
         self.final_score = ''
         self.players_in_game = set()  # set of players
         self.events_in_game = set()
-        self.penalties_in_game = set()
-        self.faceoffs_in_game = set()
-        self.shots_in_game = set()
-        self.goals_in_game = set()
+        # self.penalties_in_game = set()
+        # self.faceoffs_in_game = set()
+        # self.shots_in_game = set()
+        # self.goals_in_game = set()
         # Functions after init
         self.fetch_teams_from_game_data()
         self.retrieve_players_in_game()
         self.retrieve_events_in_game()
+        print(self.events_in_game)
 
     def fetch_teams_from_game_data(self):
         """
@@ -139,8 +56,8 @@ class Game:
         and final score
         """
         try:
-            self.away_team = self.json_data['gameData']['teams']['away']['name']
-            self.home_team = self.json_data['gameData']['teams']['home']['name']
+            self.away_team = self.json_data['gameData']['teams']['away']['triCode']
+            self.home_team = self.json_data['gameData']['teams']['home']['triCode']
             return self
         except KeyError as k:
             print(k)
@@ -150,29 +67,32 @@ class Game:
         Parse self.json_data for PLAYERS in the game
         Update self.players_in_game to be a list of [Player objects]
         """
-        try:
-            player_dict = self.json_data['gameData']['players']
-            for player in player_dict:
-                # Add all players from game
-                temp_name = player_dict[player]['fullName']  # name
-                # jersey number
-                temp_num = player_dict[player]['primaryNumber']
-                temp_team = player_dict[player]['currentTeam']['name']  # team
-                temp = Player(temp_name, temp_num, temp_team)
-                if temp not in self.players_in_game:
-                    self.players_in_game.add(temp)
-            return self.players_in_game
-        except KeyError as k:
-            print(k)
+        player_dict = self.json_data['gameData']['players']
+        for player in player_dict:
+            # Add all players from game
+            temp_name = player_dict[player]['fullName']  # name
+            # jersey number
+            temp_num = player_dict[player]['primaryNumber']
+            temp_team = player_dict[player]['currentTeam']['name']  # team
+            temp = Player(temp_name, temp_num, temp_team)
+            if temp not in self.players_in_game:
+                self.players_in_game.add(temp)
+        return self.players_in_game
 
-    def retrieve_events_in_game(self):
+
+    def parse_event(self, event):
         """
-        Parse self.json_data and retrieve all events reported in the game
-        Helper function for each type of event, since each have their own little quirks
+        Parses event from NHL API
+        Functions handle edge cases for different events
         """
-        events = self.json_data['liveData']['plays']['allPlays']
-        for event in events:
-            e_type = event['result']['event']  # Type of event
-            if e_type in self.TRACKED_EVENTS:
-                self.events_in_game.add(self.TRACKED_EVENTS[e_type](event))
-        return self.events_in_game
+        temp = Event
+        temp.player_for, temp.player_against = self.get_players(event)
+        # Blocked shot tracks WHO blocked it, not who SHOT it
+        # Must use players in game to get other team
+        temp.team_of_player = self.get_team(event)
+        temp.period = self.get_period(event)
+        temp.time = self.get_time(event)
+        temp.score = self.get_score(event)
+        temp.x_loc = self.get_x(event)
+        temp.y_locself.get_y(event)
+        return temp
